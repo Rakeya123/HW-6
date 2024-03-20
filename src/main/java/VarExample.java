88951c2024c5d572f57746f4aba6c2611f36d274
Anna Usolskaya <rakeya@yandex.ru>
2024-03-20 18:52:42 +0300

    realise1

---
+++ src/main/java/VarExample.java
@@ -0,0 +1,61 @@
+import java.beans.beancontext.BeanContextChild;
+
+public class VarExample {
+    public static void main(String[] args) {
+
+        byte aByte = 127;
+        //  byte bByte =aByte +1 ; ошибка
+        byte cByte = 111;
+        System.out.println(aByte + cByte);
+
+        short aShort = -32768;
+        //  short aShort = aShort - 1 ; ошибка
+        short cShort = 0;
+        System.out.println(aShort * cShort);
+
+
+        int aInt = 1234567890;
+        //  int bInt =aInt  +1 ; ошибка
+        int cInt = 1235;
+        System.out.println(aInt / cInt);
+
+
+        long aLong = 1234567890111L;
+        long cLong = 1235L;
+        System.out.println(aLong - cLong);
+
+        float aFloat = 10.1F;
+        float bFloat = 0.1F;
+        //float cFloat = 10.1F*10000000000; ошибка
+        System.out.println(aFloat * bFloat);
+
+
+        double aDouble = 10.10001;
+        double bDouble = 0.1;
+        System.out.println(aDouble / bDouble);
+
+
+        boolean aBoolean = true;
+        boolean bBoolean = false;
+
+        if (30 <= aByte) {
+
+            System.out.println(aBoolean);
+        } else {
+            System.out.println(bBoolean);
+        }
+
+
+
+        System.out.println(aInt / bDouble);
+        System.out.println(aDouble % 123);
+        System.out.println(aInt / bDouble*aDouble);
+
+
+
+        String sString = "Праздник";
+        System.out.println(sString.equals("Победа"));
+        System.out.println(sString.equals("Праздник") && cByte == 111);
+    }
+
+}
